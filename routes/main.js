c307ecc660b1242863a53b723b3a16d82ae12736
Monica Vinluan <52139888+mdcam@users.noreply.github.com>
2022-06-21 13:42:56 -0700

    creating routes for the controllers to follow

---
+++ routes/main.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/auth");
+const homeController = require("../controllers/home");
+const postsController = require("../controllers/posts");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
+
+//Main Routes - simplified for now
+router.get("/", homeController.getIndex);
+router.get("/profile", ensureAuth, postsController.getProfile);
+router.get("/progressionFeed", ensureAuth, postsController.getFeed);    //Changed regular feed to Project/Progression Feed
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+router.get("/logout", authController.logout);
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
+
+module.exports = router;
