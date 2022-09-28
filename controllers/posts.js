afcbcc327535e972d363fad4624b8d87704b4f45
Monica Vinluan <mdcam.prod@gmail.com>
2022-09-27 18:04:37 -0700

    formatting controller for Post

--- controllers/posts.js
+++ controllers/posts.js
@@ -12,14 +12,6 @@ module.exports = {
       console.log(err);
     }
   },
-  getFeed: async (req, res) => {
-    try {
-      const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      res.render("feed.ejs", { posts: posts });
-    } catch (err) {
-      console.log(err);
-    }
-  },
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
@@ -49,20 +41,6 @@ module.exports = {
       console.log(err);
     }
   },
-  // likePost: async (req, res) => {
-  //   try {
-  //     await Post.findOneAndUpdate(
-  //       { _id: req.params.id },
-  //       {
-  //         $inc: { likes: 1 },
-  //       }
-  //     );
-  //     console.log("Likes +1");
-  //     res.redirect(`/post/${req.params.id}`);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // },
   deletePost: async (req, res) => {
     try {
       // Find post by id
@@ -77,4 +55,26 @@ module.exports = {
       res.redirect("/profile");
     }
   },
+  // getFeed: async (req, res) => {
+  //   try {
+  //     const posts = await Post.find().sort({ createdAt: "desc" }).lean();
+  //     res.render("feed.ejs", { posts: posts });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // },
+  // likePost: async (req, res) => {
+  //   try {
+  //     await Post.findOneAndUpdate(
+  //       { _id: req.params.id },
+  //       {
+  //         $inc: { likes: 1 },
+  //       }
+  //     );
+  //     console.log("Likes +1");
+  //     res.redirect(`/post/${req.params.id}`);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // },
 };
