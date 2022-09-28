1e076071955b14d27b9236e1f44b23a3c9f167c0
Monica Vinluan <mdcam.prod@gmail.com>
2022-09-27 17:22:04 -0700

    updated header.ejs

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/database");
 
+
 //Adding routes here
-//const mainRoutes = require("./routes/main");
-//const postRoutes = require("./routes/posts");
-//const commentRoutes = require("./routes/comments");
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
+const commentRoutes = require("./routes/comments");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -40,6 +41,7 @@ app.use(logger("dev"));
 app.use(methodOverride("_method"));
 
 // Setup Sessions - stored in MongoDB
+    // store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -49,6 +51,8 @@ app.use(
   })
 );
 
+
+
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
@@ -57,9 +61,9 @@ app.use(passport.session());
 app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
-//app.use("/", mainRoutes);
-//app.use("/post", postRoutes);
-//app.use("/comment", commentRoutes);
+app.use("/", mainRoutes);
+app.use("/post", postRoutes);
+app.use("/comment", commentRoutes);
 
 //Server Running
 app.listen(process.env.PORT, () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./public/*.html"],
   theme: {
     extend: {},
   },
